349ac05e8e765389bbe2536b54b34c42ac64c241
Mounika Dummu <168990482+DummuMounika@users.noreply.github.com>
2024-05-10 23:27:30 -0500

    Time: 161 ms (26.92%), Space: 23.1 MB (64.26%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,38 +1,24 @@
 class Solution(object):
+    def rotateArray(self,nums,i,j):
+        while i <= j:
+            temp1= nums[i]
+            temp2 = nums[j]
+            nums[i] = temp2
+            nums[j] = temp1
+            i += 1
+            j -= 1
+        return nums
+
     def rotate(self, nums, k):
         l= len(nums)
         if k>l:
             k=k%l
         #for reversing whole array
-        i=0
-        j=l-1
-        while i <= j:
-            temp1= nums[i]
-            temp2= nums[j]
-            nums[i] = temp2
-            nums[j] = temp1
-            i += 1
-            j -= 1
+        nums = self.rotateArray(nums, 0,l-1) 
         #for reversing first k elements
-        i=0
-        j=k-1
-        while i <= j:
-            temp1= nums[i]
-            temp2 = nums[j]
-            nums[i] = temp2
-            nums[j] = temp1
-            i += 1
-            j -= 1
+        nums = self.rotateArray(nums, 0,k-1) 
         #for reversing remaining k elements
-        i=k
-        j=l-1
-        while i <= j:
-            temp1= nums[i]
-            temp2 = nums[j]
-            nums[i] = temp2
-            nums[j] = temp1
-            i += 1
-            j -= 1
+        nums = self.rotateArray(nums, k,l-1)
         return nums
 
         
