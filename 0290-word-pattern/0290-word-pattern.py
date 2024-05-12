f77a7beddc2b3453e19d66ac204bdab9df3360af
Mounika Dummu <168990482+DummuMounika@users.noreply.github.com>
2024-05-12 13:14:14 -0500

    Time: 12 ms (66.47%), Space: 11.7 MB (40.42%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,20 +1,9 @@
 class Solution(object):
     def wordPattern(self, pattern, s):
         s= s.split(' ')
-        print(s)
         dictS = {}
-        # dictPattern = {}
         if len(pattern) != len(s):
             return False
-        # else:
-        #     for i in range(len(pattern)):
-        #         if dictS.__contains__(pattern[i]) and dictS.get(pattern[i]) != s[i]:
-        #             return False
-        #         if dictPattern.__contains__(s[i]) and dictPattern.get(s[i]) != pattern[i]:
-        #             return False
-        #         dictS.__setitem__(pattern[i],s[i])
-        #         dictPattern.__setitem__(s[i],pattern[i])
-        #     return True
         else:
             for i in range(len(pattern)):
                 if not dictS.__contains__(pattern[i]) and s[i] not in dictS.values():
