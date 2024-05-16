429ca89f57d94c8e85574cad9e35236ed0bf5360
Mounika Dummu <168990482+DummuMounika@users.noreply.github.com>
2024-05-15 23:06:20 -0500

    Time: 19 ms (24.35%), Space: 12.1 MB (8.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,21 @@
 class Solution(object):
     
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        sdict = {'(':')', '{':'}','[':']'}
+
         stack = []
-        for c in s:
-            if c in sdict:
-                stack.append(c)
-            elif len(stack) == 0 or sdict[stack.pop()] != c:
-                return False
+        for i in range(len(s)):
+            ch = s[i]
+            if (ch == '(' or ch == '{' or ch == '['):
+                stack.append(ch)
+            else:
+                if len(stack) == 0: 
+                    return False
+                if ((stack[-1] == '(' and ch == ')') or (stack[-1] == '{' and ch == '}') 
+                or (stack[-1] == '[' and ch == ']')):
+                    stack.pop()
+                else:
+                    return False
+
         return len(stack) == 0
 
                  
