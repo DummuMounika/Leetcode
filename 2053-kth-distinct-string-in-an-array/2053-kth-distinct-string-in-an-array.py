f4374aaf5bdd49ab3a46108a8069963d09c980b2
Mounika Dummu <168990482+DummuMounika@users.noreply.github.com>
2024-08-05 15:52:05 -0500

    Time: 64 ms (84.01%), Space: 16.9 MB (7.71%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        dictk = dict()
+        for i in range(len(arr)):
+            if arr[i] not in dictk:
+                dictk[arr[i]] = 1
+            else:
+                val = 1
+                dictk[arr[i]] += val
+        print(dictk)
+        for i in range(len(arr)):
+            if dictk[arr[i]] == 1:
+                k -= 1
+            if(k==0):
+                return arr[i]
+        return ""
+                
